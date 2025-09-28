1442d2c3e87a03cd57ec995cb2fc804c2f2f58f9
youssefah3531 <youssefah3531@gmail.com>
2025-09-28 15:34:36 +0300

    main file updated

--- main_file.cpp
+++ main_file.cpp
@@ -4,100 +4,211 @@
 #include <iostream>
 using namespace std;
 string newfilename;
-void Invert_Image(string filepath )
+void Invert_Image(string filepath)
 {
     Image image(filepath);
-        for (int i = 0; i < image.width; i++)
-    {
-        for (int j = 0; j <image.height ; j++)
-        {
-            unsigned int R=0;
-            unsigned int G=0;
-            unsigned int B=0;
-            R=255-image(i,j,0);
-            G=255-image(i,j,1);
-            B=255-image(i,j,2);
-            image(i,j,0)=R;
-            image(i,j,1)=G;
-            image(i,j,2)=B;
-        }
-    }
-    cout<<"Do you want to save the photo?\n1-YES\n2-NO"<<endl;
-    int num;
-    cin>>num;
-    if (num==1)
-    {
-        cout<<"Enter the name of the new file with its extention: "<<endl;
-        cin>>newfilename;
-        image.saveImage(newfilename);
-    }
-    if (num=2)
-    {
-        cout<<"KOSOMK";
-    }
-    
-}
-void Rotate_Image(string filepath)
-{
-    int degree;
-    cout<<"How many degree do you want to rotate the photo?"<<endl;
-    cin>>degree;
-    if (degree==90)
-    {
-    Image image(filepath);
-    Image image2(image.height,image.width);
-    int k=0;
-    int l=0;
     for (int i = 0; i < image.width; i++)
     {
-        for (int j = image.height-1; j >=0; j--)
+        for (int j = 0; j < image.height; j++)
         {
-            image2(k,l,0)=image(i,j,0);
-            image2(k,l,1)=image(i,j,1);
-            image2(k,l,2)=image(i,j,2);
+            unsigned int R = 0;
+            unsigned int G = 0;
+            unsigned int B = 0;
+            R = 255 - image(i, j, 0);
+            G = 255 - image(i, j, 1);
+            B = 255 - image(i, j, 2);
+            image(i, j, 0) = R;
+            image(i, j, 1) = G;
+            image(i, j, 2) = B;
+        }
+    }
+    cout << "Do you want to save the photo?\n1-YES\n2-NO" << endl;
+    int num;
+    cin >> num;
+    if (num == 1)
+    {
+        cout << "Enter the name of the new file with its extention: " << endl;
+        cin >> newfilename;
+        image.saveImage(newfilename);
+        cout << "Image saved sucessfully\n";
+    }
+}
+void Rotate_Image_90(string filepath)
+{
+    Image image(filepath);
+    Image image2(image.height, image.width);
+    int k = 0;
+    int l = 0;
+    for (int i = 0; i < image.width; i++)
+    {
+        for (int j = image.height - 1; j >= 0; j--)
+        {
+            image2(k, l, 0) = image(i, j, 0);
+            image2(k, l, 1) = image(i, j, 1);
+            image2(k, l, 2) = image(i, j, 2);
             k++;
         }
-        k=0;
+        k = 0;
         l++;
     }
-    cout<<"Do you want to save the photo?\n1-YES\n2-NO"<<endl;
+    cout << "Do you want to save the photo?\n1-YES\n2-NO" << endl;
     int num;
-    cin>>num;
-    if (num==1)
+    cin >> num;
+    if (num == 1)
     {
-        cout<<"Enter the name of the new file with its extention: "<<endl;
-        cin>>newfilename;
-        image.saveImage(newfilename);
+        cout << "Enter the name of the new file with its extention: " << endl;
+        cin >> newfilename;
+        image2.saveImage(newfilename);
+        cout << "Image saved sucessfully\n";
     }
-    if (num=2)
+}
+void Rotate_Image_180(string filepath)
+
+{
+    Image image(filepath);
+    Image image2(image.height, image.width);
+    Image image3(image.width, image.height);
+    int k = 0;
+    int l = 0;
+    for (int i = 0; i < image.width; i++)
     {
-        cout<<"KOSOMK";
+        for (int j = image.height - 1; j >= 0; j--)
+        {
+            image2(k, l, 0) = image(i, j, 0);
+            image2(k, l, 1) = image(i, j, 1);
+            image2(k, l, 2) = image(i, j, 2);
+            k++;
+        }
+        k = 0;
+        l++;
     }
-    
+    k = 0;
+    l = 0;
+    for (int i = 0; i < image2.width; i++)
+    {
+        for (int j = image2.height - 1; j >= 0; j--)
+        {
+            image3(k, l, 0) = image2(i, j, 0);
+            image3(k, l, 1) = image2(i, j, 1);
+            image3(k, l, 2) = image2(i, j, 2);
+            k++;
+        }
+        k = 0;
+        l++;
     }
-    
+    cout << "Do you want to save the photo?\n1-YES\n2-NO" << endl;
+    int num;
+    cin >> num;
+    if (num == 1)
+    {
+        cout << "Enter the name of the new file with its extention: " << endl;
+        cin >> newfilename;
+        image3.saveImage(newfilename);
+        cout << "Image saved sucessfully\n";
+    }
+}
+void Rotate_Image_270(string filepath)
+{
+    Image image(filepath);
+    Image image2(image.height, image.width);
+    Image image3(image.width, image.height);
+    Image image4(image.height, image.width);
+    int k = 0;
+    int l = 0;
+    for (int i = 0; i < image.width; i++)
+    {
+        for (int j = image.height - 1; j >= 0; j--)
+        {
+            image2(k, l, 0) = image(i, j, 0);
+            image2(k, l, 1) = image(i, j, 1);
+            image2(k, l, 2) = image(i, j, 2);
+            k++;
+        }
+        k = 0;
+        l++;
+    }
+    k = 0;
+    l = 0;
+    for (int i = 0; i < image2.width; i++)
+    {
+        for (int j = image2.height - 1; j >= 0; j--)
+        {
+            image3(k, l, 0) = image2(i, j, 0);
+            image3(k, l, 1) = image2(i, j, 1);
+            image3(k, l, 2) = image2(i, j, 2);
+            k++;
+        }
+        k = 0;
+        l++;
+    }
+    k = 0;
+    l = 0;
+    for (int i = 0; i < image3.width; i++)
+    {
+        for (int j = image3.height - 1; j >= 0; j--)
+        {
+            image4(k, l, 0) = image3(i, j, 0);
+            image4(k, l, 1) = image3(i, j, 1);
+            image4(k, l, 2) = image3(i, j, 2);
+            k++;
+        }
+        k = 0;
+        l++;
+    }
+    cout << "Do you want to save the photo?\n1-YES\n2-NO" << endl;
+    int num;
+    cin >> num;
+    if (num == 1)
+    {
+        cout << "Enter the name of the new file with its extention: " << endl;
+        cin >> newfilename;
+        image4.saveImage(newfilename);
+        cout << "Image saved sucessfully\n";
+    }
 }
 
-
-
-int main(){
-    cout<<"#####################################################"<<endl;
-    cout<<"##########Welcome to our filter Application##########"<<endl;
-    cout<<"#####################################################"<<endl;
-    cout<<"Please load the photo (enter the full path of image): "<<endl;
-    string filepath;
-    int num;
-    getline(cin, filepath);
-    cout<<"Which filter do you want?"<<endl;
-    cout<<"1-Grey Scale\n2-Black and White\n3-Invert Image\n4-Merge Images\n5-Flip Image\n6-Rotate Image"<<endl;
-    cin>>num;
-    if (num==3)
+int main()
+{
+    string cont = "yes";
+    while (cont == "yes")
     {
-        Invert_Image(filepath);
+        cout << "#####################################################" << endl;
+        cout << "##########Welcome to our filter Application##########" << endl;
+        cout << "#####################################################" << endl;
+        cout << "Please load the photo (enter the full path of image): " << endl;
+        string filepath ="";
+        int num;
+        getline(cin >> ws, filepath);
+        cout << "Which filter do you want?" << endl;
+        cout << "1-Grey Scale\n2-Black and White\n3-Invert Image\n4-Merge Images\n5-Flip Image\n6-Rotate Image" << endl;
+        cin >> num;
+        if (num == 3)
+        {
+            Invert_Image(filepath);
+        }
+        if (num == 6)
+        {
+            int degree_rotation;
+            cout << "How many degrees do you want to rotate the image?\n";
+            cin >> degree_rotation;
+            if (degree_rotation == 90)
+            {
+                Rotate_Image_90(filepath);
+            }
+            if (degree_rotation == 180)
+            {
+                Rotate_Image_180(filepath);
+            }
+            if (degree_rotation == 270)
+            {
+                Rotate_Image_270(filepath);
+            }
+        }
+        cout << "Do you want to add another image?\n";
+        cin >> cont;
+        if (cont == "no")
+        {
+            break;
+        }
     }
-    if (num==6)
-    {
-        Rotate_Image(filepath);
-    }
-    
 }
