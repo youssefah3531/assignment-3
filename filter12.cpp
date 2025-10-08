c7f0dec9767297737627ac7a6baf01b26bd43ced
youssefah3531 <youssefah3531@gmail.com>
2025-10-08 17:03:02 +0300

    Added 9th and 12th Filters

--- filter12.cpp
+++ filter12.cpp
@@ -6,55 +6,30 @@ using namespace std;
 int main()
 {
     Image image("building.jpg");
-    for (int i = 0; i < image.width / 50; i++)
+    Image image2(image.width, image.height);
+    for (int i = 20; i < image.width-20; i++)
     {
-        for (int j = 0; j < image.height; j++)
+        for (int j = 20; j < image.height-20; j++)
         {
-            unsigned int R = 0;
-            unsigned int G = 0;
-            unsigned int B = 0;
-            image(i, j, 0) = 0;
-            image(i, j, 1) = 0;
-            image(i, j, 2) = 255;
+            int R=0;
+            int G=0;
+            int B=0;
+            for (int k = i-20; k < i+20; k++)
+            {
+                for (int l = j-20; l < j+20; l++)
+                {
+                    R+=image(k,l,0);
+                    G+=image(k,l,1);
+                    B+=image(k,l,2);
+                }
+            }
+            R/=1600;
+            G/=1600;
+            B/=1600;
+            image2(i,j,0)=R;
+            image2(i,j,1)=G;
+            image2(i,j,2)=B;
         }
     }
-    for (int i = 0; i < image.width; i++)
-    {
-        for (int j = 0; j < image.height / 70; j++)
-        {
-            unsigned int R = 0;
-            unsigned int G = 0;
-            unsigned int B = 0;
-            image(i, j, 0) = 0;
-            image(i, j, 1) = 0;
-            image(i, j, 2) = 255;
-        }
-    }
-    for (int i = image.width-1; i > image.width-70; i--)
-    {
-        for (int j = 0; j < image.height; j++)
-        {
-            unsigned int R = 0;
-            unsigned int G = 0;
-            unsigned int B = 0;
-            image(i, j, 0) = 0;
-            image(i, j, 1) = 0;
-            image(i, j, 2) = 255;
-        }
-    }
-    for (int j = image.height - 1; j >image.height-70 ; j--)
-    {
-        for (int i = image.width - 1; i > image.width / 100; i--)
-        {
-            unsigned int R = 0;
-            unsigned int G = 0;
-            unsigned int B = 0;
-            image(i, j, 0) = 0;
-            image(i, j, 1) = 0;
-            image(i, j, 2) = 255;
-        }
-    }
-
-
-    image.saveImage("iooi.png");
+    image2.saveImage("opopop.png");
 }
